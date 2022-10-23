245ee2cbb355fe62dd81ef09ac21a71475827150
Artur Petrov <artur.petrov@auto.ria.com>
2022-10-23 15:46:27 +0300

    Adding mongoose connection

--- app/app.js
+++ app/app.js
@@ -3,11 +3,15 @@ const http = require('http');
 const config = require('config');
 const routes = require('./routes/index');
 
+const mongoose = require('./modules/mongoose');
+
 const app = new Koa();
 const Router = routes.Router();
 
 app.use(Router.routes());
 app.use(Router.allowedMethods());
+
+mongoose.connect();
 
 const server = http.createServer(app.callback());
 
