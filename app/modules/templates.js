e51ea95f3ee1a1c0042ea3568d40c34345c1bfd8
Artur Petrov <artur.petrov@auto.ria.com>
2022-12-12 11:58:05 +0200

    Update

--- app/modules/templates.js
+++ app/modules/templates.js
@@ -5,6 +5,7 @@ module.exports = {
     history: fs.readFileSync('./templates/tg-bot/history.ejs', 'utf-8'),
     inventory: fs.readFileSync('./templates/tg-bot/inventory.ejs', 'utf-8'),
     profile: fs.readFileSync('./templates/tg-bot/profile.ejs', 'utf-8'),
+    primogems: fs.readFileSync('./templates/tg-bot/primogems.ejs', 'utf-8'),
     wish: fs.readFileSync('./templates/tg-bot/wish.ejs', 'utf-8'),
     wishX10: fs.readFileSync('./templates/tg-bot/wishX10.ejs', 'utf-8'),
   },
