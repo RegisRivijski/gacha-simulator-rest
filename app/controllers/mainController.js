e91845012481714870e287d6ba9360846e792921
Artur Petrov <artur.petrov@auto.ria.com>
2022-12-21 10:48:03 +0200

    Update

--- app/controllers/mainController.js
+++ app/controllers/mainController.js
@@ -1,5 +1,20 @@
 export async function start(ctx, next) {
+  ctx.body = {
 
+  };
+  ctx.status = 200;
+  await next();
+}
+
+export async function help(ctx, next) {
+  ctx.body = {
+
+  };
+  ctx.status = 200;
+  await next();
+}
+
+export async function settings(ctx, next) {
   ctx.body = {
 
   };
