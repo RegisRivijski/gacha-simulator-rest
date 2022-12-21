89504e954895fae6a68ec17939ff55b735e3b806
Artur Petrov <artur.petrov@auto.ria.com>
2022-12-21 10:54:04 +0200

    Update

--- app/controllers/mainController.js
+++ app/controllers/mainController.js
@@ -1,4 +1,19 @@
+import * as userHelper from '../helpers/usersHelper.js';
+import * as translatesHelper from '../helpers/translatesHelper.js';
+
 export async function start(ctx, next) {
+  const { chatId } = ctx.request.params;
+  ctx.assert(chatId, 400, 'chatId is required');
+
+  const userData = await userHelper.getUserData(chatId)
+    .catch((e) => {
+      console.error('[ERROR] mainController start UsersModel findOne:', e.message);
+      ctx.throw(500);
+    });
+
+  const { languageCode } = userData;
+  const $t = translatesHelper.getTranslate(languageCode);
+
   ctx.body = {
 
   };
@@ -7,6 +22,18 @@ export async function start(ctx, next) {
 }
 
 export async function help(ctx, next) {
+  const { chatId } = ctx.request.params;
+  ctx.assert(chatId, 400, 'chatId is required');
+
+  const userData = await userHelper.getUserData(chatId)
+    .catch((e) => {
+      console.error('[ERROR] mainController help UsersModel findOne:', e.message);
+      ctx.throw(500);
+    });
+
+  const { languageCode } = userData;
+  const $t = translatesHelper.getTranslate(languageCode);
+
   ctx.body = {
 
   };
@@ -15,6 +42,18 @@ export async function help(ctx, next) {
 }
 
 export async function settings(ctx, next) {
+  const { chatId } = ctx.request.params;
+  ctx.assert(chatId, 400, 'chatId is required');
+
+  const userData = await userHelper.getUserData(chatId)
+    .catch((e) => {
+      console.error('[ERROR] mainController settings UsersModel findOne:', e.message);
+      ctx.throw(500);
+    });
+
+  const { languageCode } = userData;
+  const $t = translatesHelper.getTranslate(languageCode);
+
   ctx.body = {
 
   };
