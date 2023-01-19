13d9205eadcef1b79b76f0b76a7f8799766ac00c
Artur Petrov <artur.petrov@auto.ria.com>
2023-01-19 17:51:17 +0200

    Update

--- app/modules/minify.js
+++ app/modules/minify.js
@@ -1,16 +1,13 @@
 import htmlMinify from 'html-minifier';
 
-export function minifyTgBot(messageTemplate) {
-  return htmlMinify.minify(messageTemplate, {
-    collapseWhitespace: true,
-    removeTagWhitespace: true,
-  })
-    .replaceAll('<br>', '\n');
-}
-
 export function minify(messageTemplate) {
   return htmlMinify.minify(messageTemplate, {
     collapseWhitespace: true,
     removeTagWhitespace: true,
   });
+}
+
+export function minifyTgBot(messageTemplate) {
+  return minify(messageTemplate)
+    .replaceAll('<br>', '\n');
 }
