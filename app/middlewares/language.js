59c1f95007a8a92047f302088c9851f3e7cbf89f
Artur Petrov <artur.petrov@auto.ria.com>
2023-01-25 12:36:06 +0200

    Update

--- app/middlewares/language.js
+++ app/middlewares/language.js
@@ -1,8 +1,6 @@
 import config from '../../config/default.js';
 
 export async function getDefaultLangCode(ctx, next) {
-  const defaultLangCode = ctx.headers['x-default-langcode'] || config.languages.defaultLangCode;
-  global.defaultLangCode = defaultLangCode;
-  ctx.state.defaultLangCode = defaultLangCode;
+  ctx.state.defaultLangCode = ctx.headers['x-default-langcode'] || config.languages.defaultLangCode;
   await next();
 }
