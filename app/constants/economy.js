a4817f44d78293ee2db29db8a4d84175832e5661
arturpetrov <petrov0397@gmail.com>
2024-12-25 19:03:34 +0200

    Changing referral reward

--- app/constants/economy.js
+++ app/constants/economy.js
@@ -3,6 +3,6 @@ export const STARDUST_NAME = 'stardust';
 export const STARGLITTER_NAME = 'starglitter';
 
 export const START_PRIMOGEMS_COUNT = 9000;
-export const PRIMOGEMS_REFERRAL_REWARD = 6000;
+export const PRIMOGEMS_REFERRAL_REWARD = 20000;
 export const PRIMOGEMS_GET_MAX = 1600;
 export const PRIMOGEMS_COEFFICIENT = 0.9;
