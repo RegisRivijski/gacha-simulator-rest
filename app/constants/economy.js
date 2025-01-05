e1e022403a3631758ce9927958e469b095421023
arturpetrov <petrov0397@gmail.com>
2025-01-05 15:21:29 +0200

    Decreasing PRIMOGEMS_REFERRAL_REWARD

--- app/constants/economy.js
+++ app/constants/economy.js
@@ -4,7 +4,7 @@ export const STARGLITTER_NAME = 'starglitter';
 
 export const START_PRIMOGEMS_COUNT = 9000;
 
-export const PRIMOGEMS_REFERRAL_REWARD = 50000;
+export const PRIMOGEMS_REFERRAL_REWARD = 15000;
 
 export const PRIMOGEMS_GET_MAX = 1600;
 export const PRIMOGEMS_COEFFICIENT = 0.9;
